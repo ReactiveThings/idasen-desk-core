e06a64d37896726de7c5812c9b7d20778964f6d3
Paweł Milka <pawel.milka@aerdata.com>
2025-10-02 23:50:53 +0200

    mydesk

--- src/Idasen.ConsoleApp/Program.cs
+++ src/Idasen.ConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System ;
 using System.IO ;
+using System.Linq;
+using System.Reactive.Linq;
 using System.Threading ;
 using System.Threading.Tasks ;
 using Autofac ;
@@ -16,7 +18,7 @@ namespace Idasen.ConsoleApp
         /// <summary>
         ///     Test Application
         /// </summary>
-        private static async Task Main ( )
+        private static async Task Main (string[] args)
         {
             var tokenSource = new CancellationTokenSource ( TimeSpan.FromSeconds ( 60 ) ) ;
             var token       = tokenSource.Token ;
@@ -35,16 +37,33 @@ namespace Idasen.ConsoleApp
 
             var (isSuccess , desk) = await provider.TryGetDesk ( token ) ;
 
-            if ( isSuccess )
-                desk!.MoveTo ( 7200u ) ;
-            else
-                logger.Error ( "Failed to detect desk" ) ;
+            if (args.Length == 1)
+            {
+                var height = uint.Parse(args[0]);
+                if (height < 6300u)
+                {
+                    height = 6600u;
+                }
+                if (height > 11800u)
+                {
+                    height = 11800u;
+                }
+                if (isSuccess)
+                    desk!.MoveTo(height);
+                else
+                    logger.Error("Failed to detect desk");
 
-            ReadLine ( ) ;
+                desk.FinishedChanged.Subscribe(p => System.Environment.Exit(0));
+            }
+
+            desk.MoveLock();
+            //
+            Console.ReadKey();
+
         }
 
-        private const string DefaultDeviceName              = "Desk" ;
-        private const ulong  DefaultDeviceAddress           = 250635178951455u ;
+        private const string DefaultDeviceName              = "Desk 2295" ;
+        private const ulong  DefaultDeviceAddress           = 235696085912647u ;
         private const uint   DefaultDeviceMonitoringTimeout = 600u ;
     }
 }
